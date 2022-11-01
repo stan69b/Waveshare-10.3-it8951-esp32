9ed1e3a07c681a383bd9007ebd5214022f87b72e
Stan Bono <stanislas.bonneau@thestouch.com>
2022-11-01 14:53:58 +0100

    fixes + multiple simulator rendering algo

--- it8951/display.ino
+++ it8951/display.ino
@@ -14,7 +14,7 @@ uint8_t display_begin() {
 void display_buffer(uint8_t* addr, uint32_t x, uint32_t y, uint32_t w, uint32_t h, int mode = 2) {
     gpFrameBuf = addr;
     Serial.println("Displaying image");
-    IT8951_BMP_Example(x, y, w, h); // needed to draw images at any positions other than 0,0
-    IT8951DisplayArea(x, y, w, h, mode);
-    Serial.println("done");
+    IT8951_BMP_Example(x -= x%4, y, w, h); // needed to draw images at any positions other than 0,0
+    IT8951DisplayArea(x -= x%4, y, w, h, mode); //x -= x%4, as stated in the original repo seems to be necessary to display images in position other than 0,0
+    Serial.println("done");                     // If not present (x -= x%4), rendered lines of the image will have a distortien to it ..... weird.
 }
